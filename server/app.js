d0eee6d03331319329c77499f5bddf620947c36c
Nitish Matiyara <91205231+NitishMatiyara@users.noreply.github.com>
2023-09-25 00:56:09 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -28,19 +28,9 @@ app.use(router);
 
 // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.send("API is running..");
   });
-}
 
 // --------------------------deployment------------------------------
 
